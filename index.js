659d4e67e717d26cbe9b2f89cd939ae4412d747c
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-03-01 22:12:46 +1100

    assignment submitted

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {name:'barry', streetAdress:'burke road, deepdene'};
+
+function updateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+    const newemployee = { ...employee};
+    newemployee[name] = streetAddress; 
+    return newemployee;
+  }
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+    employee[name]= streetAddress;
+    return employee;
+  }
+
+
+function deleteFromEmployeeByKey(employee, name) {
+    const newemployee = { ...employee};
+    delete newemployee.name
+    return newemployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee.name
+    return employee
+}
